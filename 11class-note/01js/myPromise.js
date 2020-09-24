f8fc1136f4e4eac60f65689d42a594af228ec04b
SedationH <sedationh@gmail.com>
2020-09-24 11:03:03 +0800

    ⚡️ Promise同步逻辑

--- 11class-note/01js/myPromise.js
+++ 11class-note/01js/myPromise.js
@@ -11,15 +11,17 @@ class MyPromise {
   reason = undefined
 
   // 注意使用箭头函数，调用的函数是直接使用resolve的，需要绑定当前实例的this
+  // 否则拿到的this可能是window | undefined(stric)
   resolve = value => {
+    if (this.status !== PENDING) return
     this.status = FULFILLED
     this.value = value
-    console.dir(this)
   }
 
-  reject() {
+  reject = reason => {
+    if (this.status !== PENDING) return
     this.status = REJECTED
-    console.log(this)
+    this.reason = reason
   }
 
   then(onFulfilled, onRejected) {
--- 11class-note/01js/useMyPromise.js
+++ 11class-note/01js/useMyPromise.js
@@ -2,7 +2,7 @@ const { reject } = require('lodash/fp')
 const MyPromise = require('./myPromise')
 
 const promise = new MyPromise((resolve, reject) => {
-  // resolve('success')
+  resolve('success')
   reject('fail')
 }).then(
   value => console.log(value),
