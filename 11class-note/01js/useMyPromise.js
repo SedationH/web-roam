52b78421ded4eea4cf70491b8c4b968225116831
SedationH <sedationh@gmail.com>
2020-09-24 11:12:20 +0800

    ⚡️ 解决单个then的异步

--- 11class-note/01js/useMyPromise.js
+++ 11class-note/01js/useMyPromise.js
@@ -1,9 +1,9 @@
-const { reject } = require('lodash/fp')
 const MyPromise = require('./myPromise')
 
 const promise = new MyPromise((resolve, reject) => {
-  resolve('success')
-  reject('fail')
+  setTimeout(() => {
+    resolve('success')
+  }, 0)
 }).then(
   value => console.log(value),
   reason => console.log(reason)
