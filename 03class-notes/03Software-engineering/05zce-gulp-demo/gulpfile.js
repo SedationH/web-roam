ddeb75e6bfa7f70f30f3880ec424568764d1ebbf
SedationH <sedationh@gmail.com>
2020-10-16 17:07:09 +0800

    ⚡️ go on

--- 03class-notes/03Software-engineering/05zce-gulp-demo/gulpfile.js
+++ 03class-notes/03Software-engineering/05zce-gulp-demo/gulpfile.js
@@ -1,6 +1,7 @@
-const { parallel, series, src, dest } = require('gulp')
+const { parallel, series, src, dest, watch } = require('gulp')
 
 const del = require('del')
+const { stream } = require('browser-sync')
 
 // f2 rename
 // const plugins.scss = require('gulp-sass')
@@ -9,17 +10,21 @@ const del = require('del')
 // const plugins.imageMin = require('gulp-imagemin')
 const plugins = require('gulp-load-plugins')()
 // 更新命名方式: 驼峰 if gulp-a-bc -> plugins.aBc
+// 开发服务器 hot reload
+const bs = require('browser-sync').create()
+
+
 
 const style = () =>
   src('src/assets/**/*.scss', { base: 'src' })
     // 默认不处理_开头的
     .pipe(plugins.sass())
-    .pipe(dest('dist'))
+    .pipe(dest('temp'))
 
 const script = () =>
   src('src/**/*.js', { base: 'src' })
     .pipe(plugins.babel({ presets: ['@babel/preset-env'] }))
-    .pipe(dest('dist'))
+    .pipe(dest('temp'))
 
 const data = {
   menus: [
@@ -65,8 +70,9 @@ const data = {
 const page = () =>
   // 这里约定src根目录下的html文件才需要转换
   src('src/*.html', { base: 'src' })
-    .pipe(plugins.swig(data))
-    .pipe(dest('dist'))
+    .pipe(plugins.swig({ data, defaults: { cache: false } }))
+    .pipe(dest('temp'))
+// .pipe(bs.reload({ stream: true)})
 
 // 压缩图片
 const image = () =>
@@ -81,14 +87,73 @@ const font = () =>
 
 const extra = () =>
   src('public/**', { base: 'public' })
-    .pipe(dest('dist/public'))
+    .pipe(dest('dist'))
 
-const clean = () => del('dist')
+const clean = () => del(['temp', 'dist'])
 
-const compile = parallel(style, script, page, image, font)
-const build = series(clean, parallel(compile, extra))
+const serve = () => {
+  watch('src/assets/styles/*.scss', style)
+
+  watch('src/assets/scripts/*.js', script)
+  // watch('src/*.html', page)
+  // 也可以写成每个类型自动reload brower 注意看page实现
+  watch('src/*.html', page)
+
+
+  // watch('src/assets/ images/**', image)
+  // watch('src/assets/fonts/**', font)
+  // watch('public/**', extra)
+  watch([
+    'src/assets/images/**',
+    'src/assets/fonts/**',
+    'public/**'
+  ], bs.reload)
+
+  bs.init({
+    // notify: false,
+    port: 2080,
+    files: 'temp/**',
+    server: {
+      // 如果在dist中不存在就向下一个找
+      baseDir: ['temp', 'src', 'public'],
+      routes: {
+        // 如果读取到/node_modules的请求，进行重定向
+        '/node_modules': 'node_modules'
+      },
+    },
+  })
+}
+
+// 相同文件同时读写会有一方无法完成，注意分开
+const useref = () =>
+  src('temp/*.html', { base: 'temp' })
+    // 补充ref的时候寻找位置
+    .pipe(plugins.useref({ searchPath: ['temp', '.'] }))
+    .pipe(plugins.if(/\.js$/, plugins.uglify()))
+    .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
+    .pipe(plugins.if(/\.html$/, plugins.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    })))
+    .pipe(dest('dist'))
+
+const compile = parallel(style, script, page)
+
+const build = series(
+  clean,
+  parallel(
+    series(compile, useref),
+    image,
+    font,
+    extra,
+  )
+)
+const develop = series(compile, serve)
+// 一些重要的思考：平衡开发效率和上线效率
 
 module.exports = {
   compile,
   build,
+  develop
 }
